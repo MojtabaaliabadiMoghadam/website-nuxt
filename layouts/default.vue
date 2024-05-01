5377af29604daa34c3a5a0724c83ef5697fb5443
Sadr IT <mojiland2002@gmail.com>
2024-05-01 21:14:57 +0330

    add tailwind to project

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Header/>
+    <div>
+      <slot />
+    </div>
+    <Footer/>
+  </div>
+</template>
+<script lang="ts" setup>
+
+</script>
+
+<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  app: {
+    head: {
+      meta: [
+        // <meta name="viewport" content="width=device-width, initial-scale=1">
+        {name: 'viewport', content: 'width=device-width, initial-scale=1'}
+      ],
+      script: [
+        {src: 'https://unpkg.com/flowbite@1.4.5/dist/flowbite.js'}
+      ],
+      link: [],
+      // please note that this is an area that is likely to change
+      style: [],
+      noscript: []
+    },
+  },
 })
