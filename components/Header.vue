dbcf99b9bed97e47bf58b38f81c2804c2f0f9735
Sadr IT <mojiland2002@gmail.com>
2024-05-01 22:01:40 +0330

    fix header style and hover that

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="w-[200px] h-[300px] text-red-500 bg-amber-300">
-    this is a header
+  <div class="w-full h-[64px] text-white bg-[#003e25] flex justify-center items-center gap-8">
+    <div v-for="(header,index) in items_headers" :key="index" class="flex h-full hover:bg-[#fff3ab] items-center justify-center w-[110px] mx-1">
+      <span class="font-bold text-[16px] text-nowrap">{{header.title}}</span>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
+const items_headers = reactive([
+  {title : 'کارت سبز'},
+  {title : 'نتایج لاتاری'},
+  {title : 'مشاوره برندگان'},
+  {title : 'مشاوره مهاجرت'},
+  {title : 'رضایت مشتری'},
+  {title : 'مجوزها'},
+  {title : 'مجله'},
+  {title : 'درباره ما'},
+  {title : 'تماس با ما'},
+])
+console.log(items_headers)
 </script>
